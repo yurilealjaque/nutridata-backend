71f9b7fe3e38a7d030f6aff740de78f8c253bc3d
jpizarroc99 <jpizarroc99@gmail.com>
2025-08-05 21:33:30 -0400

    Cambio a ingles de todo el package de producto

--- src/main/java/com/example/nutridata/product/Product.java
+++ src/main/java/com/example/nutridata/product/Product.java
@@ -4,28 +4,16 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Entity
-@Table(name="producto")
+@Table(name = "product")
 @Getter @Setter
 public class Product {
 
     @Id                                       // Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id_producto;
-    @Column(name = "nombre")
-    private String nombre;
-    private String descripcion;
-    private int precio;
+    private Integer id;
+    private String name;
+    private String description;
+    private int price;
     private int stock;
-
-
-    private int categoria_id;
-    private boolean activo;
-    private LocalDate fecha_creacion;
-
-    public Product() {
-
-    }
 }
